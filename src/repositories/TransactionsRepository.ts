863964b5054ed164fbd3084d82184eb4a5cb3dce
Mark Fazolin <markfazolin@yahoo.com.br>
2020-06-27 12:12:31 -0300

    Finished challenge 004

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,63 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDto {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
+
+  public typeIncome = 'income';
+
+  public typeOutcome = 'outcome';
 
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  public isValidType(type: string): boolean {
+    return type === this.typeIncome || type === this.typeOutcome;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const initialValue = 0;
+
+    const income = this.transactions.reduce((accumulator, transaction) => {
+      let tmpValue = 0;
+
+      if (transaction.type === this.typeIncome) tmpValue = transaction.value;
+
+      return accumulator + tmpValue;
+    }, initialValue);
+
+    const outcome = this.transactions.reduce((accumulator, transaction) => {
+      let tmpValue = 0;
+
+      if (transaction.type === this.typeOutcome) tmpValue = transaction.value;
+
+      return accumulator + tmpValue;
+    }, initialValue);
+
+    const total = income - outcome;
+
+    const balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDto): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,30 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!this.transactionsRepository.isValidType(type)) {
+      throw new Error(
+        `Invalid type '${type}' to create transaction. Use 'income' or 'outcome'.`,
+      );
+    }
+
+    if (type === this.transactionsRepository.typeOutcome) {
+      const balance = this.transactionsRepository.getBalance();
+
+      if (value > balance.total) {
+        throw new Error(
+          'The value of the new outgoing transaction is greater than your balance.',
+        );
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
